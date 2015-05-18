5fb6aeeb0dcc9541c4f3aa9ea1f704e16b7a4c9a
Martin Liu <hualiu@ebay.com>
2015-05-18 18:17:19 +0800

    fix test

--- clj/test/clj/core_test.clj
+++ clj/test/clj/core_test.clj
@@ -1,10 +1,7 @@
 (ns clj.core-test
   (:require [clojure.test :refer :all]
-            [clj.main :refer :all]
-            [clj.macro :refer :all]
-            [clj.type :refer :all]
             ))
 
 (deftest test-1
   (testing "FIXME, I fail."
-    (is (= 1 2))))
+    (is (= 1 1))))
