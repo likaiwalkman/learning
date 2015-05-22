16bb53f8bb3a0c5ef2b128282b8e472f81a73ad8
Martin Liu <hualiu@ebay.com>
2015-05-22 18:30:26 +0800

    save progress

--- sicp/src/sicp/core.clj
+++ sicp/src/sicp/core.clj
@@ -1,4 +1,5 @@
-(ns sicp.core)
+(ns sicp.core
+  (:require [clojure.pprint :as pp]))
 
 (defn sqrt-newton
   "Newton method to calculate sqrt"
@@ -24,3 +25,30 @@
      (fib pre (+ pree pre) (- n 1)))))
 
 ;; counting change
+;; coins: 1, 5, 10, 25, 50
+(defn count-change
+  ([amount] (count-change amount [50 25 10 5 1]))
+  ([amount coins]
+   (cond
+     ;; 0 => 1, so that 5 has 2 kinds of change
+     (= amount 0) 1
+     (< amount 0) 0
+     (= 1 (count coins)) (if (zero? (mod amount (first coins)))
+                           1
+                           0)
+     :else (+
+            ;; without first coin
+            (count-change amount (rest coins))
+            ;; with first coin
+            (count-change (- amount (first coins)) coins)))))
+
+;; pascal's triangle
+(defn pascal-triangle
+  [level]
+  (if (= level 1)
+    [[1]]
+    (let [pre (pascal-triangle (- level 1))
+          pre-last (last pre)]
+      (conj pre (vec (map #(+ (or (get pre-last (dec %)) 0)
+                              (or (get pre-last %) 0))
+                          (range (inc (count pre-last)))))))))
--- sicp/test/sicp/core_test.clj
+++ sicp/test/sicp/core_test.clj
@@ -7,3 +7,12 @@
       (format "%.4f" (sqrt-newton 2.0)) => "1.4142")
 (fact "fib could be work"
       (fib 6) => 8)
+
+(fact "about `count-change'"
+      (count-change 1) => 1
+      (count-change 2) => 1
+      (count-change 5) => 2
+      (count-change 7) => 2
+      (count-change 10) => 4
+      (count-change 100) => 292
+      )
